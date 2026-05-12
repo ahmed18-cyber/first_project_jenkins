749e8685bc029e0245c0de572291658c101d2efc
billie <billie@ahmad.myguest.virtualbox.org>
2026-05-12 03:40:42 +0100

    Premiere script Jenkins

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("=" * 40 )
+print("Bienvenue dans mon premiere job Jenkins !")
+print("=" * 40 )
+
+nom = input(" Quel est ton nom ?")
+print(f"Bonjour{nom}, ton job Jenkins a reussi !")
+
+# petit calcul 
+a = 10
+b = 5
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+
